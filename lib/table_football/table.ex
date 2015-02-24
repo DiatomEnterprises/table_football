d1af96a44f826dd9cc10c1333cd52b8b182a4535
Janis Miezitis <janjiss@gmail.com>
2015-02-24 10:47:40 +0200

    Refactored table process

--- lib/table_football/table.ex
+++ lib/table_football/table.ex
@@ -10,18 +10,31 @@ defmodule TableFootball.Table do
     GenServer.cast(__MODULE__, {:register_player, id})
   end
 
-  def handle_cast({:register_player, id}, state) do
-    new_state = case state do
-      %{left_player_id: nil, game_pid: nil} ->
-        add_left_player(state, id)
-      %{left_player_id: ^id, right_player_id: ^id, game_pid: nil} ->
-        state
-      %{right_player_id: nil, game_pid: nil} ->
-        add_right_player_and_spawn_game(state, id)
-      %{game_pid: _pid} ->
-        state
-    end
+  def handle_cast({:register_player, id}, %{game_pid: game_pid} = state) when is_pid(game_pid) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:register_player, id}, %{left_player_id: nil, game_pid: nil} = state) do
+    new_state = add_left_player(state, id)
     {:noreply, new_state}
+  end
+
+  def handle_cast({:register_player, id}, %{left_player_id: id, game_pid: nil} = state) do
+    {:noreply, state}
+  end
+
+  def handle_cast({:register_player, id}, %{right_player_id: nil, game_pid: nil} = state) do
+    new_state = add_right_player_and_spawn_game(state, id)
+    {:noreply, new_state}
+  end
+
+  def add_left_player(state, id) do
+    Dict.merge(state, %{left_player_id: id})
+  end
+
+  def add_right_player_and_spawn_game(state = %{left_player_id: left_player_id}, id) do
+    {:ok, game_pid} = TableFootball.GameLogic.start_link(left_player_id: left_player_id, right_player_id: id)
+    Dict.merge(state, %{right_player_id: id, game_pid: game_pid})
   end
 
   def get_state do
@@ -32,13 +45,4 @@ defmodule TableFootball.Table do
     {:reply, state, state}
   end
 
-  def add_left_player(state, id) do
-    Dict.merge(state, %{left_player_id: id})
-  end
-
-  def add_right_player_and_spawn_game(state, id) do
-    %{left_player_id: left_player_id} = state
-    {:ok, game_pid} = TableFootball.GameLogic.start_link(left_player_id: left_player_id, right_player_id: id)
-    Dict.merge(state, %{right_player_id: id, game_pid: game_pid})
-  end
 end
