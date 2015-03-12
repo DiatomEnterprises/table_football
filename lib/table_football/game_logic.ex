7aa416081443f5ec630ce1ef1543d25b4cc585a8
Janis Miezitis <janjiss@gmail.com>
2015-03-12 15:28:45 +0200

    Small refactoring to game logic

--- lib/table_football/game_logic.ex
+++ lib/table_football/game_logic.ex
@@ -19,28 +19,36 @@ defmodule TableFootball.GameLogic do
   end
 
   def handle_info({:score, :right}, %Game{right_score: 9} = game_state) do
-    new_game_state = %{game_state | right_score: game_state.right_score + 1}
+    new_game_state = add_right_score(game_state)
     EventBus.notify(:victory, new_game_state)
     EventBus.unsubscribe(self, :score)
     {:stop, :normal, new_game_state}
   end
 
   def handle_info({:score, :left}, %Game{left_score: 9} = game_state) do
-    new_game_state = %{game_state | left_score: game_state.left_score + 1}
+    new_game_state = add_left_score(game_state)
     EventBus.notify(:victory, new_game_state)
     EventBus.unsubscribe(self, :score)
     {:stop, :normal, new_game_state}
   end
 
   def handle_info({:score, :right}, game_state) do
-    new_game_state = %{game_state | right_score: game_state.right_score + 1}
+    new_game_state = add_right_score(game_state)
     EventBus.notify(:game_update, new_game_state)
     {:noreply, new_game_state}
   end
 
   def handle_info({:score, :left}, game_state) do
-    new_game_state = %{game_state | left_score: game_state.left_score + 1}
+    new_game_state = add_left_score(game_state)
     EventBus.notify(:game_update, new_game_state)
     {:noreply, new_game_state}
+  end
+
+  defp add_left_score(game_state) do
+    %{game_state | left_score: game_state.left_score + 1}
+  end
+
+  defp add_right_score(game_state) do
+    %{game_state | right_score: game_state.right_score + 1}
   end
 end
